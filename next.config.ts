7ac1265e9b3236c40b895474cabdffb82e61e547
YeuThuyr <thien@LAPTOP-F1GRRMUV>
2025-03-07 21:31:00 +0700

    final update

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/Thuyr",
-  assetPrefix: "/Thuyr/",
+  basePath: '/Thuyr',
+  assetPrefix: '/Thuyr/',
   output: 'export',
   distDir: 'dist',
   images: {
