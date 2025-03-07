8800c6720ac3f2d0a767274fff9161c0279b6f12
YeuThuyr <thien@LAPTOP-F1GRRMUV>
2025-03-07 21:50:53 +0700

    final update

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
-const isPro = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: isPro ? '/Thuyr' : '',
   output: "export",
   distDir: 'dist',
   images: {
